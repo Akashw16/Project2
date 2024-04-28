75cfc116ee418007ceea852cd6f988f88e1f6e2c
Akashw16 <iamakashw16@gmail.com>
2024-04-28 23:14:18 +0530

    updated

---
+++ LastActivity.java
@@ -0,0 +1,14 @@
+package com.example.myintroapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_last);
+    }
+}
